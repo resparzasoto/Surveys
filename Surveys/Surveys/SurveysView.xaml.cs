9370a109de27c0410e2d953a9d9f8b7fe3ffbf70
Jesus Ruben Esparza Soto <jesus.esparza@coppel.com>
2019-01-18 11:43:30 -0700

    Capitulo: 5 Página: 140 Descripción: se agrega el enlace de datos a la aplicación.

--- Surveys/Surveys/SurveysView.xaml.cs
+++ Surveys/Surveys/SurveysView.xaml.cs
@@ -15,14 +15,6 @@ namespace Surveys
 		public SurveysView ()
 		{
 			InitializeComponent ();
-
-            MessagingCenter.Subscribe<ContentPage, Survey>(this, Messages.NewSurveyComplete, (sender, args) =>
-            {
-                SurveysPanel.Children.Add(new Label()
-                {
-                    Text = args.ToString()
-                });
-            });
 		}
 
         private async void AddSurveyButton_Clicked(object sender, EventArgs e)
